b386a08d3d9c4cbe2bedfceda7dc72c50ce60158
sopholunar <916177960@qq.com>
2025-02-18 13:01:16 +0800

    新增员工——代码开发2（在专业版2023.3.4idea完成）

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -20,6 +20,8 @@ public interface EmployeeMapper {
      * 插入员工数据
      * @param employee
      */
-    @Insert("insert into employee")
+    @Insert("insert into employee (name, username, password, phone, sex, id_number, status, create_time, update_time, create_user, update_user) " +
+            "values " +
+            "(#{name}, #{username}, #{password}, #{phone}, #{sex}, #{id_number}, #{status}, #{creat_time}, #{update_time}, #{create_user}, #{update_user})")
     void insert(Employee employee);
 }
